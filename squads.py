14a9794c15f14b69006b2b40d8c293a15f979f80
Ben Schreiber <benobi1995@gmail.com>
2018-06-28 14:40:55 -0600

    Add algorithm to balance squads

--- squads.py
+++ squads.py
@@ -52,3 +52,114 @@ class Squad(PlayerList):
         writeHTMLToFile(html, fn)
 
         return html
+
+
+def bitReverse(value, maxValue):
+    """
+    Get the reverse of the binary representation of a given value
+    """
+    if value > maxValue:
+        raise ValueError(("Given value of %d is greater than given "
+                          "maximum value of %d" % (value, maxValue)))
+    bitLengthDiff = maxValue.bit_length()-value.bit_length()
+    bits = '0'*bitLengthDiff + bin(value)[2:]
+    reversedBits = bits[::-1]
+    return int(reversedBits, 2)
+
+
+def tournamentRank(players):
+    """
+    Rank the players as if they were in a single elimintion tournament
+
+    This ranking is accomplished by sorting the players by the sum
+    of their three skills. The rankings are converted into their
+    binary representation with the same number of bits as the total
+    number of player, left padded with zeros, whjch are then reversed
+    to generate their 'slot' in the tournament. Due to the nature of
+    this conversion, some numbers may be converted into invalid slots,
+    so the players are simply added to the tournament list in order
+    of increasing 'slot'.
+    """
+    tournament = []
+    sortedPlayers = sorted(players, reverse=True,
+                           key=(lambda x: x.skate+x.shoot+x.check))
+
+    tournamentPlayers = []
+    for i, player in enumerate(sortedPlayers):
+        slot = bitReverse(i+1, len(players))
+        tournamentPlayers.append((player, slot))
+
+    slottedTournament = sorted(tournamentPlayers, key=(lambda x: x[1]))
+    for slottedPlayer in slottedTournament:
+        tournament.append(slottedPlayer[0])
+
+    return tournament
+
+
+def getBalancedSquads(numSquads, playerList):
+    """
+    Dynamically generate psuedo balanced squads of players from a given list
+
+    Algorithm to balance teams is based on https://stackoverflow.com/a/1363601.
+    Players are ranked by the sum of their three skills, and this ranking is
+    used to generate their place in a single-elimination tournament. Once the
+    players are placed in this tournament order, each team alternates taking
+    the top and bottom players of the tournament into their team, until all
+    teams have reached the maximum number of players possible given the
+    desired number of teams. Remaining players are left on the waiting list.
+
+    Errors if the number of desired squads is greater than the number of
+    available players, or if number of desired squads is less than one.
+
+    Special Cases:
+        1 squad:    When only one squad is needed, all players are moved from
+                    the waiting list into the squad.
+
+        numPlayers squads:  When the number of players is equal to the number
+                            of desired squads, each player is placed onto their
+                            own squad, bypassing the need for calculations
+
+    TODO: Rank separately for each of the skills rather than using the sum.
+    FUTURE OPTIMIZATION: Once a roughly balanced team is generated, go through
+    and attempt to balance further by swapping players between teams
+    """
+    squads = []
+    numPlayers = len(playerList.players)
+    if(numSquads > numPlayers):
+        raise ValueError(("Number of Squads cannot be greater then number of "
+                          "players. %d squads attempted, %d players available."
+                          % (numSquads, numPlayers)))
+    elif(numSquads < 1):
+        raise ValueError(("Number of Squads must be greater than one,"
+                          "%d given" % numSquads))
+    elif(numSquads == 1):
+        newSquad = Squad(1, playerList.players)
+        squads.append(newSquad)
+        newSquad.toHTML()
+        playerList.players = []
+    elif(numSquads == numPlayers):
+        for i in range(numPlayers):
+            newSquad = Squad(i+1, [playerList.players.pop()])
+            squads.append(newSquad)
+            newSquad.toHTML()
+    else:
+        squadSize = floor(numPlayers/numSquads)
+        tourney = tournamentRank(playerList.players)
+
+        squadPlayers = [[] for i in range(numSquads)]
+        for i in range(squadSize):
+            for squad in squadPlayers:
+                if(len(squad) % 2 == 0):
+                    pick = tourney.pop(0)
+                else:
+                    pick = tourney.pop()
+                squad.append(pick)
+                playerList.players.remove(pick)
+
+        for i, squad in enumerate(squadPlayers):
+            newSquad = Squad(i+1, squad)
+            squads.append(newSquad)
+            newSquad.toHTML()
+
+    playerList.toHTML()
+    return squads
--- test/test_squads.py
+++ test/test_squads.py
@@ -2,7 +2,7 @@ import squads
 
 from math import floor
 from unittest import TestCase
-from unittest.mock import mock_open, patch
+from unittest.mock import MagicMock, mock_open, patch
 
 from players import Player, PlayerList, PlayerTable
 
@@ -34,3 +34,164 @@ class TestSquad(TestCase):
 
         self.assertEqual(html, expectedHTML)
         self.assertEqual(squad.players, outputPlayers)
+
+
+class TestBitReverse(TestCase):
+
+    def testBitReverse(self):
+        value = 3           # Binary representation 011
+        expectedValue = 6   # Binary representation 110
+        maxValue = 7        # Binary representation 111
+
+        revValue = squads.bitReverse(value, maxValue)
+        self.assertEqual(revValue, expectedValue)
+
+    def testBitReverse__oneToMax(self):
+        value = 1       # Binary representation 0001, reverses to 1000
+        maxValue = 8    # Binary representation 1000
+
+        revValue = squads.bitReverse(value, maxValue)
+        self.assertEqual(revValue, maxValue)
+
+    def testBitReverse__oneToOne(self):
+        value = 1       # Binary representation 1
+        maxValue = 1
+        self.assertEqual(squads.bitReverse(value, maxValue), value)
+
+    def testBitReverse__raisesValueError(self):
+        value = 9
+        maxValue = 8
+
+        with self.assertRaises(ValueError):
+            squads.bitReverse(value, maxValue)
+
+
+class testTournamentRank(TestCase):
+
+    def testTournamentRank(self):
+        player1 = Player("99", "Wayne Gretzky", 99, 99, 99)     # Becomes 4
+        player2 = Player("97", "Connor McDavid", 97, 97, 97)    # Becomes 2
+        player3 = Player("123", "Ben Schreiber", 51, 51, 51)    # Becomes 6
+        player4 = Player("456", "Another Skater", 42, 42, 42)   # Becomes 1
+        player5 = Player("789", "That Guy", 31, 31, 31)         # Becomes 5
+        player6 = Player("404", "Nota RealPlayer", 20, 20, 20)  # Becomes 3
+
+        players = [player1, player2, player3, player4, player5, player6]
+        expectedTournament = [player4, player2, player6,
+                              player1, player5, player3]
+        tournament = squads.tournamentRank(players)
+        self.assertEqual(tournament, expectedTournament)
+
+    def testTournamentRank__single(self):
+        player = Player("123", "Ben Schreiber", 51, 51, 51)
+        self.assertEqual([player], squads.tournamentRank([player]))
+
+    def testTournamentRank__doubleSwap(self):
+        player1 = Player("99", "Wayne Gretzky", 99, 99, 99)
+        player2 = Player("123", "Ben Schreiber", 51, 51, 51)
+
+        players = [player1, player2]
+        expectedTournament = [player2, player1]
+        tournament = squads.tournamentRank(players)
+        self.assertEqual(tournament, expectedTournament)
+
+    def testTournamentRankSorts(self):
+        """
+        Test that the tournament ranker sorts the given players before slotting
+        """
+        player1 = Player("123", "Ben Schreiber", 51, 51, 51)
+        player2 = Player("99", "Wayne Gretzky", 99, 99, 99)
+
+        players = [player1, player2]
+        expectedTournament = [player1, player2]
+        tournament = squads.tournamentRank(players)
+        self.assertEqual(tournament, expectedTournament)
+
+
+class TestGetBalancedSquads(TestCase):
+
+    @patch('players.PlayerList.toHTML', MagicMock)
+    @patch('squads.Squad.toHTML', MagicMock)
+    def testGetBalancedSquads(self):
+        numSquads = 2
+        player1 = Player("99", "Wayne Gretzky", 99, 99, 99)
+        player2 = Player("97", "Connor McDavid", 97, 97, 97)
+        player3 = Player("123", "Ben Schreiber", 51, 51, 51)
+        player4 = Player("404", "Nota RealPlayer", 20, 20, 20)
+        playerList = PlayerList([player1, player2, player3, player4])
+
+        expectedSquads = [squads.Squad(1, [player4, player3]),
+                          squads.Squad(2, [player2, player1])]
+
+        balSquads = squads.getBalancedSquads(numSquads, playerList)
+
+        self.assertEqual(balSquads, expectedSquads)
+
+    @patch('players.PlayerList.toHTML', MagicMock)
+    @patch('squads.Squad.toHTML', MagicMock)
+    def testGetBalancedSquads__OneSquad(self):
+        numSquads = 1
+        player1 = Player("99", "Wayne Gretzky", 99, 99, 99)
+        player2 = Player("97", "Connor McDavid", 97, 97, 97)
+        player3 = Player("123", "Ben Schreiber", 51, 51, 51)
+        player4 = Player("404", "Nota RealPlayer", 20, 20, 20)
+        playerList = PlayerList([player1, player2, player3, player4])
+
+        expectedSquads = [squads.Squad(1, playerList.players)]
+
+        balSquads = squads.getBalancedSquads(numSquads, playerList)
+        self.assertEqual(balSquads, expectedSquads)
+        self.assertEqual(playerList.players, [])
+
+    @patch('players.PlayerList.toHTML', MagicMock)
+    @patch('squads.Squad.toHTML', MagicMock)
+    def testGetBalancedSquads__NSquads(self):
+        numSquads = 4
+        player1 = Player("99", "Wayne Gretzky", 99, 99, 99)
+        player2 = Player("97", "Connor McDavid", 97, 97, 97)
+        player3 = Player("123", "Ben Schreiber", 51, 51, 51)
+        player4 = Player("404", "Nota RealPlayer", 20, 20, 20)
+        playerList = PlayerList([player1, player2, player3, player4])
+
+        expectedSquads = [squads.Squad(1, [player4]),
+                          squads.Squad(2, [player3]),
+                          squads.Squad(3, [player2]),
+                          squads.Squad(4, [player1])]
+
+        balSquads = squads.getBalancedSquads(numSquads, playerList)
+
+        self.assertEqual(balSquads, expectedSquads)
+        self.assertEqual(playerList.players, [])
+
+    def testGetBalancedSquadsRaisesValueError__GreaterThanNSquads(self):
+        numSquads = 5
+        player1 = Player("99", "Wayne Gretzky", 99, 99, 99)
+        player2 = Player("97", "Connor McDavid", 97, 97, 97)
+        player3 = Player("123", "Ben Schreiber", 51, 51, 51)
+        player4 = Player("404", "Nota RealPlayer", 20, 20, 20)
+        playerList = PlayerList([player1, player2, player3, player4])
+
+        with self.assertRaises(ValueError):
+            balSquads = squads.getBalancedSquads(numSquads, playerList)
+
+    def testGetBalancedSquadsRaisesValueError__ZeroSquads(self):
+        numSquads = 0
+        player1 = Player("99", "Wayne Gretzky", 99, 99, 99)
+        player2 = Player("97", "Connor McDavid", 97, 97, 97)
+        player3 = Player("123", "Ben Schreiber", 51, 51, 51)
+        player4 = Player("404", "Nota RealPlayer", 20, 20, 20)
+        playerList = PlayerList([player1, player2, player3, player4])
+
+        with self.assertRaises(ValueError):
+            balSquads = squads.getBalancedSquads(numSquads, playerList)
+
+    def testGetBalancedSquadsRaisesValueError__NegSquads(self):
+        numSquads = -1
+        player1 = Player("99", "Wayne Gretzky", 99, 99, 99)
+        player2 = Player("97", "Connor McDavid", 97, 97, 97)
+        player3 = Player("123", "Ben Schreiber", 51, 51, 51)
+        player4 = Player("404", "Nota RealPlayer", 20, 20, 20)
+        playerList = PlayerList([player1, player2, player3, player4])
+
+        with self.assertRaises(ValueError):
+            balSquads = squads.getBalancedSquads(numSquads, playerList)
