6135e1ff7bf1c82f4da762653c04fb06864e23f7
Ben Schreiber <benobi1995@gmail.com>
2018-06-28 18:39:53 -0600

    Generate Dynamic Squads on Button Press

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+import json
+
+from flask import Flask, render_template, request
 
 from players import PlayerList
+from squads import getBalancedSquads
 
 app = Flask(__name__)
 
@@ -16,8 +19,26 @@ def main():
     return render_template('index.html')
 
 
+@app.route("/squads", methods=["POST"])
+def generateSquads():
+    """
+    Generate a number of balanced squads equivalent to the user's input from
+    the given player data, and enable them to be displayed on the new page.
+    """
+    numSquads = int(request.form["numSquads"])
+    playerList = PlayerList.fromJSON()
+    squads = getBalancedSquads(numSquads, playerList)
+    squadsHTML = [("squad%d.html" % (i+1)) for i in range(numSquads)]
+
+    return render_template('index.html', squads=squadsHTML)
+
+
 if __name__ == "__main__":
     """
     When calling this file in the command line, run the application
+
+    Configures the application to reload templates if their contents change,
+    to avoid old data from persisting after updating.
     """
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run()
